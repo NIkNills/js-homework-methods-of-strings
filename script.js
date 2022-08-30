54065c3bafd4187db97c5d973c5ab82133aea6f0
Nik Kolesnikov <niknills94@gmail.com>
2022-08-30 18:53:22 +0300

    feat: added task 2 solution

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function stringCutter(str) {
 console.log(stringCutter(firstSentence));
 console.log(stringCutter(secondSentence));
 console.log(stringCutter(thirdSentence));
+
+function registerInvertion(str) {
+  return str
+    .split("")
+    .map((item) =>
+      item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()
+    )
+    .join("");
+}
+
+console.log(registerInvertion("WTF"));
+console.log(registerInvertion("Be my Valentine"));
+console.log(registerInvertion("C&C Red Alert 3"));
